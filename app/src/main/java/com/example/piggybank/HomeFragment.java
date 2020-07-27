e7ca5782558f9cdf01de54c955ee8df41ea3ca71
saharkh <skhazali99@gmail.com>
2020-07-27 15:20:38 +0430

    ASC

--- app/src/main/java/com/example/piggybank/HomeFragment.java
+++ app/src/main/java/com/example/piggybank/HomeFragment.java
@@ -3,15 +3,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 public class HomeFragment extends Fragment {
     View view;
+    Button expense,income;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.home_fragment, container, false);
+        findView();
+        expense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ExpenseFragment bottomFragment = new ExpenseFragment();
+                bottomFragment.show(getFragmentManager(), bottomFragment.getTag());
+            }
+        });
+        income.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ExpenseFragment bottomFragment = new ExpenseFragment();
+                bottomFragment.show(getFragmentManager(), bottomFragment.getTag());
+            }
+        });
         return view;
+    }
+
+    private void findView() {
+        expense=view.findViewById(R.id.new_expense);
+        income=view.findViewById(R.id.new_income);
     }
 }
---
+++ app/src/main/java/com/example/piggybank/IncomeFragment.java
@@ -0,0 +1,17 @@
+package com.example.piggybank;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class IncomeFragment extends BottomSheetDialogFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.income_fragment, container, false);
+    }
+}
